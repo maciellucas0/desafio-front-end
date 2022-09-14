68057750ff9d387f5863a303a44079e5941b3da9
Lucas Maciel <lucasm.contato@hotmail.com>
2022-09-14 15:42:57 -0300

    feat/captacao dos dados completa

--- desafio/src/components/formCadastro/index.tsx
+++ desafio/src/components/formCadastro/index.tsx
@@ -1,63 +1,87 @@
-import { BsFillEyeSlashFill } from "react-icons/bs";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { BsFillEyeSlashFill, BsMinecart } from "react-icons/bs";
 import { RiFileList2Fill } from "react-icons/ri";
 import { Formulario } from "./style";
 
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { ContextCadastro } from "../../contexts/contextCadastro";
+import { useContext } from "react";
 
 const FormCadastro = () => {
   const navigate = useNavigate();
 
-  const [senhaPrimaria, setSenhaPrimaria] = useState("password");
-  const [senhaSecundaria, setSenhaSecundaria] = useState("password");
+  const formSchema = yup.object().shape({
+    nome: yup.string().required("Nome obrigatório"),
+    cpf: yup.string().required("CPF obrigatório").min(11).max(11),
+    nascimento: yup.string().required("Data de nascimento obrigatória"),
+    email: yup.string().required("email obrigatório").email(),
+    senha: yup.string().required("Senha obrigatória"),
+    confirmarSenha: yup
+      .string()
+      .oneOf([yup.ref("senha"), null], "As senhas precisam ser iguais."),
+    checkbox: yup.boolean().notRequired(),
+  });
 
-  function VerSenhaPrimaria() {
-    if (senhaPrimaria === "password") {
-      setSenhaPrimaria("text");
-    } else {
-      setSenhaPrimaria("password");
-    }
-  }
-  function VerSenhaSecundaria() {
-    if (senhaSecundaria === "password") {
-      setSenhaSecundaria("text");
-    } else {
-      setSenhaSecundaria("password");
-    }
-  }
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  /*------CONTEXTO--------*/
+  const {
+    onSubmit,
+    senhaPrimaria,
+    senhaSecundaria,
+    VerSenhaPrimaria,
+    VerSenhaSecundaria,
+  } = useContext(ContextCadastro);
 
   return (
     <>
-      <Formulario>
+      <Formulario onSubmit={handleSubmit(onSubmit)}>
         <h1>Quero me cadastrar</h1>
 
-        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Nome" {...register("nome")} />
         <div className="row">
-          <input type="number" placeholder="CPF" />
+          <input type="number" placeholder="CPF" {...register("cpf")} />
           <fieldset>
             <legend>Data de nascimento</legend>
-            <input type="date" id="noBorder" />
+            <input type="date" id="noBorder" {...register("nascimento")} />
           </fieldset>
         </div>
-        <input type="email" placeholder="E-mail" />
+        <input type="email" placeholder="E-mail" {...register("email")} />
         <div className="senha">
-          <input type={senhaPrimaria} placeholder="Crie sua senha" />
+          <input
+            type={senhaPrimaria}
+            placeholder="Crie sua senha"
+            {...register("senha")}
+          />
           <button type="button" onClick={() => VerSenhaPrimaria()}>
             <BsFillEyeSlashFill />
           </button>
         </div>
         <div className="senha">
-          <input type={senhaSecundaria} placeholder="Confirme sua senha" />
+          <input
+            type={senhaSecundaria}
+            placeholder="Confirme sua senha"
+            {...register("confirmarSenha")}
+          />
           <button onClick={() => VerSenhaSecundaria()} type="button">
             <BsFillEyeSlashFill />
           </button>
         </div>
         <div className="rowCheckbox">
-          <input type="checkbox" id="checkbox"></input>
+          <input
+            type="checkbox"
+            id="checkbox"
+            {...register("checkbox")}
+          ></input>
           <span>Voce aceita receber comunicações por e-mail?</span>
         </div>
 
-        <button id="cadastrar">
+        <button type="submit" id="cadastrar">
           <RiFileList2Fill />
           CADASTRAR
         </button>
