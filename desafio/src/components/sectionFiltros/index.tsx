8e9a9f60d6e720f44b79a8f4f0b0f13754b5666c
Lucas Maciel <lucasm.contato@hotmail.com>
2022-09-18 10:46:10 -0300

    feat/captura dados in put

--- desafio/src/components/sectionFiltros/index.tsx
+++ desafio/src/components/sectionFiltros/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { FcSearch } from "react-icons/fc";
 import { StyledSection } from "./style";
+
 export const SectionFilter = () => {
+  const [busca, setBusca] = useState("");
   return (
     <>
       <StyledSection>
         <div id="container">
           <h1>Arquivados</h1>
           <div>
-            <input id="title" type="text" placeholder="Título ou ID" />
+            <input
+              value={busca}
+              onChange={(event) => setBusca(event.target.value)}
+              id="title"
+              type="text"
+              placeholder="Título ou ID"
+            />
             <span>
               <FcSearch />
             </span>
